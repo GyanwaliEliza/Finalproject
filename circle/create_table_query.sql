2a7e8669da7f3351e678ff59c759b4ba591248ca
BKroos <btkroos@gmail.com>
2021-09-12 15:28:11 -0500

    Adding updated database schema and ERD

--- circle/create_table_query.sql
+++ circle/create_table_query.sql
@@ -1,28 +1,32 @@
 CREATE TABLE calls (
 	sr_no VARCHAR NOT NULL,
 	sr_type VARCHAR NOT NULL,
+	department TEXT,
 	sr_method TEXT,
 	sr_status TEXT,
-	sr_change TIMESTAMP,
-	sr_created TIMESTAMP,
-	sr_update TIMESTAMP,
-	sr_close TIMESTAMP,
-	sr_location TEXT,
-	st_number VARCHAR,
-	st_name TEXT,
-	city TEXT,
 	zip_code VARCHAR(5),
+	district INT,
+	city TEXT,
 	county TEXT,
+	sr_year INT,
+	open_time INTERVAL,
+	open_time_d INTERVAL,
+	total_seconds INTERVAL,
+	sr_created TIMESTAMP,
+	sr_created_dt TIMESTAMP,
+	sr_close TIMESTAMP,
+	sr_close_dt TIMESTAMP,
+	sr_change TIMESTAMP,
+	sr_update TIMESTAMP,
+	map_pg VARCHAR,
+	map_tile VARCHAR,
 	state_x VARCHAR,
 	state_y VARCHAR,
 	lat VARCHAR,
 	long VARCHAR,
 	lat_long POINT,
-	district INT,
-	map_pg VARCHAR,
-	map_tile VARCHAR);
+	sr_location TEXT,
+	st_number VARCHAR,
+	st_name TEXT
+	);
 	
-SELECT COUNT(sr_no) FROM 
-	calls
-WHERE 
-	(zip_code = '78704');

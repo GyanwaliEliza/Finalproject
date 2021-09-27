b904fad2afcf01ba739a392e628744ac92ade273
Mateo Rul <mrul@sunpowercorp.com>
2021-09-26 21:04:48 -0500

    Unable to trouble shoot leaflet markers not being drawn

--- Dashboard/map_script.js
+++ Dashboard/map_script.js
@@ -1,18 +1,34 @@
+mymap = L.map('mapid').setView([30.2729 , -97.7444], 12.25);
 
-var mymap = L.map('mapid').setView([30.2729 , -97.7444], 12.25);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,  
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicnVsbWEiLCJhIjoiY2tyMWFta3h0MjBneDMxcWh3N2c4djE4YSJ9.oJy_15dF1Y0JoP0x25O15w'
+}).addTo(mymap);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoicnVsbWEiLCJhIjoiY2tyMWFta3h0MjBneDMxcWh3N2c4djE4YSJ9.oJy_15dF1Y0JoP0x25O15w'
-    }).addTo(mymap);
+function buildMap(data){
 
-    var call_marker  = L.marker([30.1860228,-97.83034442], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(mymap);
+        data.forEach( (call) => {
+
+            console.log(call.sr_location_lat, call.sr_location_long)
+
+            if (parseFloat(call.sr_location_lat) && parseFloat(call.sr_location_long)){
+                    let mark = L.marker([parseFloat(call.sr_location_lat), parseFloat(call.sr_location_long)], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 500
+                    });
+
+                    mark.addTo(mymap);
+
+                    console.log(mark);
+                    console.log(mymap);
+            }
+
+        })
+
+};
